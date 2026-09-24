ac2f1fb58a9185747d5a3c25558b9a0fc91bd7bd
Piyush_changole <piyushchangolechangole078@gmail.com>
2026-09-24 21:29:33 +0530

    Updated calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
 using namespace std; 
 int main(){
-    int a,b;
-    char op;
-    cout<<"Enter a and b:";
-    cin>>a>>b;
-    cout<<"Enter the operator :";
-    cin>>op;
-    if(op == '+'){
-        cout<<a+b;
-    }else if(op  == '-'){
-        cout<<a-b;
-    }else if(op == '*'){
-        cout<<a*b;
-    }else if (op == '/'){
-        cout<<a/b;
-    }else{
-        cout<<"INvalid operator!";
-    }
+   cout<<"MY first github update!";
     return 0;
 
 }
